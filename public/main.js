28f0a3f77c994a161a5625bce5801f71fcd01a51
Akram Sukor <akramsukor@gmail.com>
2023-01-12 00:58:41 +0800

    Mobile draft

--- public/main.js
+++ public/main.js
@@ -122,7 +122,34 @@ window.onclick = function(e) {
     }
 };
 
+//light slider------------------------------------------
 
+$(document).ready(function() {
+    $('#responsive').lightSlider({
+        item:3,
+        loop:false,
+        slideMove:2,
+        easing: 'cubic-bezier(0.25, 0, 0.25, 1)',
+        speed:600,
+        responsive : [
+            {
+                breakpoint:800,
+                settings: {
+                    item:2,
+                    slideMove:1,
+                    slideMargin:6,
+                  }
+            },
+            {
+                breakpoint:500,
+                settings: {
+                    item:1,
+                    slideMove:1
+                  }
+            }
+        ]
+    });  
+  });
 
 // GSAP Animation
 
