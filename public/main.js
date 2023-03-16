9e600e8a83a83a4f6e6d0ec60f9a87f3fdd19ff5
Akram Sukor <akramsukor@gmail.com>
2023-03-16 23:12:49 +0800

    Structure

--- public/main.js
+++ public/main.js
@@ -151,6 +151,52 @@ $(document).ready(function() {
     });  
   });
 
+//testimonial slider-----------------------------------------------------------------------
+
+const carouselSlide2 = document.querySelector('.carousel-slide2');
+const carouselImages2 = document.querySelectorAll('.carousel-slide2-image img');
+
+const prevBtn2 = document.querySelector('#prevBtn2');
+const nextBtn2 = document.querySelector('#nextBtn2');
+
+let counter2 = 1;
+const size2 = carouselImages2[0].clientWidth;
+
+carouselSlide2.style.transform = 'translateX(' + (-size2*counter2) + 'px)';
+
+nextBtn2.addEventListener('click',()=>{
+    if(counter2 >= carouselImages2.length - 1) return;
+    carouselSlide2.style.transition = 'transform 0.7s ease-in-out';
+    counter2++;
+    carouselSlide2.style.transform = 'translateX(' + (-size2*counter2) + 'px)';
+});
+
+prevBtn2.addEventListener('click',()=>{
+    if(counter2 <= 0) return;
+    carouselSlide2.style.transition = 'transform 0.7s ease-in-out';
+    counter2--;
+    carouselSlide2.style.transform = 'translateX(' + (-size2*counter2) + 'px)';
+});
+
+carouselSlide2.addEventListener('transitionend', ()=>{
+    if (carouselImages2[counter2].id === 'lastClone2'){
+        carouselSlide2.style.transition = 'none';
+        counter2 = carouselImages2.length - 2;
+        carouselSlide2.style.transform = 'translateX(' + (-size2*counter2) + 'px)';
+    }
+    if (carouselImages2[counter2].id === 'firstClone2'){
+        carouselSlide2.style.transition = 'none';
+        counter2 = carouselImages2.length - counter2;
+        carouselSlide2.style.transform = 'translateX(' + (-size2*counter2) + 'px)';
+    }
+});
+
+setTimeout(function() {
+    setInterval(function() {
+        document.getElementById("nextBtn2").click();
+    }, 5000);
+}, 5000);  
+
 // GSAP Animation
 
 // gsap.to(".mentor-img", {
